1565663c4ca674dab68bc155e6c15103de871f58
oscarnicolas <oscarnicolasmozo@gmail.com>
2022-05-31 23:02:11 -0500

    flash loan executed

---
+++ config.js
@@ -0,0 +1,12 @@
+// Mainnet DAI Address
+const DAI = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
+// Random user's address that happens to have a lot of DAI on Polygon Mainnet
+const DAI_WHALE = "0x6e53cb6942e518376e9e763554db1a45ddcd25c4";
+
+// Mainnet Pool contract address
+const POOL_ADDRESS_PROVIDER = "0xa97684ead0e402dc232d5a977953df7ecbab3cdb";
+module.exports = {
+  DAI,
+  DAI_WHALE,
+  POOL_ADDRESS_PROVIDER,
+};
